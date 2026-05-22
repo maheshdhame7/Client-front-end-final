3613405aeeac28bde27157c3a025945ed16e3b44
Mahesh Dhame <139204475+maheshdhame7@users.noreply.github.com>
2026-05-23 00:42:10 +0530

    Bug fix
    Bug fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,29 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
-// Attach JWT token to every request
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-// Auto-logout on 401
-API.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-      window.location.hash = "#/login";
-    }
-    return Promise.reject(err);
-  }
-);
 
 export default API;
