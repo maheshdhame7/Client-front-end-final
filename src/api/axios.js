f857fe3c270649ac48ee130ff8ef19a812837f7f
Mahesh Dhame <139204475+maheshdhame7@users.noreply.github.com>
2026-05-23 01:00:06 +0530

    URL bug fix
    URL bug fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: "https://team-task-api-production-7c6f.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
